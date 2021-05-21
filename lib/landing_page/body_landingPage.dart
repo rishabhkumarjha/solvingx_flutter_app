55573cfc1a8266c504f960bd10244f63aea05a52
Rishabh Jha <rishabhkumarjha@gmail.com>
2021-05-21 13:32:57 +0530

    added horizontal scroll

--- lib/landing_page/body_landingPage.dart
+++ lib/landing_page/body_landingPage.dart
@@ -1,8 +1,50 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Body");
+    List<String> imgList = [
+      "https://live.staticflickr.com/2896/33583627230_066841a2fb_b.jpg",
+      "https://live.staticflickr.com/7158/6629088361_602f6c9736_b.jpg"
+    ];
+    return ListView(children: [
+      Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Featured",
+              style: TextStyle(color: Colors.orange, fontSize: 26),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height / 3,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: imgList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.network(
+                        imgList[index],
+                        fit: BoxFit.cover,
+                        width: MediaQuery.of(context).size.width >
+                                MediaQuery.of(context).size.height
+                            ? MediaQuery.of(context).size.width * 0.25
+                            : MediaQuery.of(context).size.width * 0.7,
+                      ),
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      )
+    ]);
   }
 }
